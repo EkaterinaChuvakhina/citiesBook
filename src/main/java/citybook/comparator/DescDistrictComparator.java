07ab2e160b4ed13d1a6162b64eaee6717974b72d
Ekaterina Chuvahina <chuvahina20@gmail.com>
2021-05-19 17:23:37 +0700

    fixed DescDistrictComparator

--- src/main/java/citybook/comparator/DescDistrictComparator.java
+++ src/main/java/citybook/comparator/DescDistrictComparator.java
@@ -7,6 +7,6 @@ import java.util.Comparator;
 public class DescDistrictComparator implements Comparator<City> {
     @Override
     public int compare(City o1, City o2) {
-        return -o1.getDistrict().compareToIgnoreCase(o2.getDistrict());
+        return o1.getDistrict().compareTo(o2.getDistrict());
     }
 }
